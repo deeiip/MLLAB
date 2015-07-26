c0fa0df0399a9421f325e6bac5acfeaeafc7f704
deeiip <ping@dipanjan.me>
2015-07-26 08:05:00 +0530

    preped with gtest

---
+++ MLStudio.core.Tests/MLStudio.core.Tests.cpp
@@ -0,0 +1,19 @@
+// MLStudio.core.Tests.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "gtest\gtest.h"
+#include "..\MLStudio.core\ExecutionPlan.h"
+
+
+//int _tmain(int argc, _TCHAR* argv[])
+//{
+//	return 0;
+//}
+
+TEST(testMath, myCubeTest)
+{
+	ExecutionPlan ex;
+	EXPECT_EQ(1000, 1000);
+	
+}
